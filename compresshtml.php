7735b1324fa81390fc51738da5646e7e0965a183
vsevjednom-cz <56153843+vsevjednom-cz@users.noreply.github.com>
2021-11-02 12:59:58 +0100

    Create compresshtml.php

---
+++ compresshtml.php
@@ -0,0 +1,20 @@
+function compressHTML($buffer) {
+
+    $search = array(
+        '/\>[^\S ]+/s',     // strip whitespaces after tags, except space
+        '/[^\S ]+\</s',     // strip whitespaces before tags, except space
+        '/(\s)+/s',         // shorten multiple whitespace sequences
+        '/<!--(.|\s)*?-->/' // Remove HTML comments
+    );
+
+    $replace = array(
+        '>',
+        '<',
+        '\\1',
+        ''
+    );
+
+    $buffer = preg_replace($search, $replace, $buffer);
+
+    return $buffer;
+}
